b8d9103be0eb3dca5f161b16b8f7540bbed0ac04
Eric Tornabell Noguera <etornabell@idiapjgol.info>
2022-11-18 15:47:39 +0100

    first commit

---
+++ build.data/datatreatment.R
@@ -0,0 +1,36 @@
+library(data.table)
+library(lubridate)
+library(readr)
+library(readODS)
+library(tidyverse)
+library(dplyr)
+# Carreguem dades
+conditions <- readRDS('~idiap/data/scorewater/scorewater2/SCOREwater_entregable_conditions_20221110.rds')
+farmacs <- readRDS('~idiap/data/scorewater/scorewater2/SCOREwater_entregable_farmacs_20221110.rds')
+groups <- readRDS('~idiap/data/scorewater/scorewater2/SCOREwater_entregable_grups_ICD10MC_20221110.rds')
+barrisbesos=read_rds("/home/idiap/projects/scorewater/seccions_censals_barris.rds") %>% select(Zona, Sc2020) %>% rename(scensal=Sc2020)
+
+# Per cada arxiu afegim un 0 davant dels codis censals curts i marquem quins d'ells pertanyen als barris de BCN-besos d'interès
+setDT(conditions)
+conditions$scensal = stringr::str_pad(conditions$scensal, 10, side = "left", pad = 0)
+conditions$cm<-NULL
+conditions$ip2011<-NULL
+conditions = left_join(conditions, barrisbesos)
+
+setDT(farmacs)
+farmacs$scensal = stringr::str_pad(farmacs$scensal, 10, side = "left", pad = 0)
+farmacs$cm<-NULL
+farmacs$ip2011<-NULL
+farmacs = left_join(farmacs, barrisbesos)
+
+setDT(groups)
+groups$scensal = stringr::str_pad(groups$scensal, 10, side = "left", pad = 0)
+groups$cm<-NULL
+groups$ip2011<-NULL
+groups = left_join(groups, barrisbesos)
+
+saveRDS(conditions, file = 'build.data/conditions.rds')
+
+saveRDS(farmacs, file = 'build.data/farmacs.rds')
+
+saveRDS(groups, file = 'build.data/groups.rds')
---
+++ build.data/poblacioreal.R
@@ -0,0 +1,39 @@
+library(data.table)
+library(lubridate)
+library(readr)
+library(readODS)
+library(tidyverse)
+
+# Carreguem dades
+conditions <- readRDS('~/idiap/projects/scorewater2/build.data/conditions.rds')
+farmacs <- readRDS('~/idiap/projects/scorewater2/build.data/farmacs.rds')
+groups <- readRDS('~/idiap/projects/scorewater2/build.data/groups.rds')
+
+# Llegim les taules amb les dades provinents de l'INE de l'any 2021 i les ajuntem totes en un unic fitxer
+barcelona = read.table('~/idiap/projects/scorewater2/poblacio_BCN_2021.csv', sep='\t', header = TRUE) %>% rename(edat = Edad..grupos.quinquenales.) %>% rename(scensal=Sección)
+girona = read.table('~/idiap/projects/scorewater2/poblacio_GIR_2021.csv', sep='\t', header = TRUE) %>% rename(edat = Edad..grupos.quinquenales.) %>% rename(scensal=Sección)
+tarragona = read.table('~/idiap/projects/scorewater2/poblacio_TAR_2021.csv', sep='\t', header = TRUE) %>% rename(edat = Edad..grupos.quinquenales.) %>% rename(scensal=Sección)
+lleida = read.table('~/idiap/projects/scorewater2/poblacio_LLE_2021.csv', sep='\t', header = TRUE) %>% rename(edat = Edad..grupos.quinquenales.) %>% rename(scensal=Sección)
+# Les dades de sidiap només arriben fins a 80+ anys, aixi que tots els superiors els cambiem per aquesta categoria
+catalunya = rbind(barcelona, girona, tarragona, lleida) %>% mutate(edat=ifelse(edat=='100 y más'|edat=='95-99'|edat=='90-94'|edat=='85-89'|edat=='80-84', '80+', edat))
+setDT(catalunya)
+# Eliminem les files que contenen dades que no ens interessen com el totals
+catalunya = catalunya[edat!='Total']
+catalunya = catalunya[scensal!='TOTAL']
+# Sumem tots els membres de les categories 80+ generades anteriorment
+catalunya$Total<-as.numeric(catalunya$Total)
+catalunya = catalunya %>% group_by(Sexo, scensal, edat) %>% summarize_if(is.numeric, sum)
+setDT(catalunya)
+# Arreglem la variable sexe
+catalunya = catalunya[Sexo!='Ambos Sexos']
+catalunya = catalunya %>% mutate(Sexo=ifelse(Sexo=='Hombres', 'H', 'D')) %>% rename(sexe = Sexo)
+
+# Ajuntem dades
+conditions = left_join(conditions, catalunya) %>% rename(pob_real=Total)
+farmacs = left_join(farmacs, catalunya) %>% rename(pob_real=Total)
+groups = left_join(groups, catalunya) %>% rename(pob_real=Total)
+
+# I ja estaria
+saveRDS(conditions, file = 'build.data/conditionsreals.rds')
+saveRDS(farmacs, file = 'build.data/farmacsreals.rds')
+saveRDS(groups, file = 'build.data/groupsreals.rds')
